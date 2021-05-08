d06d837bb8e39dfe5713c6c6ff79dd1451f8b2cd
Timur Aniyatov <timuraniyatov@gmail.com>
2021-05-08 10:06:39 +0600

    Решил задачу 3 модуль 2

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,17 @@
 let calculator = {
   // ваш код
+  a: '',
+  b: '',
+  read(a, b) {
+    this.a = a;
+    this.b = b;
+  },
+  sum() {
+    return this.a + this.b;
+  },
+  mul() {
+    return this.a * this.b;
+  }
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
